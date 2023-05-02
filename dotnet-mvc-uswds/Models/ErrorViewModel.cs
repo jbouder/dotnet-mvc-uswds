64897c15ce70ed4cd02f9b95f98f604b4d47b0e7
Jonathan Bouder <jbouder@metrostar.com>
2023-05-02 19:38:25 -0400

    Initial commit.

---
+++ dotnet-mvc-uswds/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace dotnet_mvc_uswds.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
