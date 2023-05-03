8de8560cd91ca7d093c72d8e7adec014b7c25898
Jonathan Bouder <jbouder@metrostar.com>
2023-05-02 20:17:22 -0400

    Add dashboard. Update navigation.

---
+++ dotnet-mvc-uswds/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_mvc_uswds.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
